09e9f699a545862aa9dd296b2cb64fbc52ce4ac8
lyra <darby.vranek@gmail.com>
2018-07-11 17:42:23 -0700

    setting up models etc

--- aurora_rp/aurora_rp/settings.py
+++ aurora_rp/aurora_rp/settings.py
@@ -37,11 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'characters',
-    'imgs',
-    'posts',
-    'threads',
-    'verses'
+    'apps.rp'
 ]
 
 MIDDLEWARE = [
@@ -80,8 +76,10 @@ WSGI_APPLICATION = 'aurora_rp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'rp',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'admin',
+        'PASSWORD': 'admin'
     }
 }
 
