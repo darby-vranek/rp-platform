50212c71a63cbe946aac18fc390c01cea152afc6
lyra <darby.vranek@gmail.com>
2018-07-12 21:15:55 -0700

    I believe everything is back to normal

--- aurora_rp/apps/rp/models.py
+++ aurora_rp/apps/rp/models.py
@@ -16,6 +16,9 @@ class Entity(Model):
     caption = models.CharField(max_length=255, default='', blank=True)
     desc = models.TextField(blank=True)
 
+    def __str__(self):
+        return self.display_name
+
     class Meta:
         abstract = True
 
@@ -24,9 +27,6 @@ class Character(Entity):
     page_name = models.CharField(max_length=30)
     sm_icon = models.URLField(default='', blank=True)
     lg_icon = models.URLField(default='', blank=True)
-
-    def __str__(self):
-        return self.display_name
 
     def get_absolute_url(self):
         return reverse('character-detail', kwargs={'pk': self.pk})
@@ -37,11 +37,30 @@ class CharacterForm(ModelForm):
         model = Character
         fields = ['page_name', 'display_name', 'caption', 'desc', 'sm_icon', 'lg_icon']
 
+
+class Verse(Entity):
+    franchise = models.CharField(max_length=255)
+
+    def get_absolute_url(self):
+        return reverse('verse-detail', kwargs={'pk': self.pk})
+
+
+class VerseForm(ModelForm):
+    class Meta:
+        model = Verse
+        fields = ['display_name', 'caption', 'desc', 'franchise']
+
+
 class Bio(Entity):
-    char = models.ForeignKey(Character, related_name='bios', on_delete=models.DO_NOTHING, null=True)
-    ver = models.ForeignKey(Verse, related_name='bios', on_delete=models.DO_NOTHING, null=True)
+    character = models.ForeignKey(Character, related_name='bios', on_delete=models.DO_NOTHING)
+    verse = models.ForeignKey(Verse, related_name='bios', on_delete=models.DO_NOTHING)
     sm_icon = models.URLField(default='', blank=True)
     lg_icon = models.URLField(default='', blank=True)
 
     def get_absolute_url(self):
         return reverse('bio-detail', kwargs={'pk': self.pk})
+
+
+class BioForm(ModelForm):
+    model = Bio
+    fields = ['character', 'verse', 'display_name', 'caption', 'desc', 'sm_icon', 'lg_icon']
