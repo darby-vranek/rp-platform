5c491de54653251577c2d62ce9aa30fbb1816370
lyra <darby.vranek@gmail.com>
2018-07-14 00:35:48 -0700

    reformatted

---
+++ apps/rp/models.py
@@ -0,0 +1,131 @@
+from django.db import models
+from django.forms import ModelForm
+from django.urls import reverse
+
+
+class Model(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Entity(Model):
+    display_name = models.CharField(max_length=255)
+    caption = models.CharField(max_length=255, default='', blank=True)
+    desc = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.display_name
+
+    class Meta:
+        abstract = True
+
+
+class Character(Entity):
+    page_name = models.CharField(max_length=30)
+    sm_icon = models.URLField(default='', blank=True)
+    lg_icon = models.URLField(default='', blank=True)
+    verses = models.ManyToManyField('Verse', through='Bio', symmetrical=True, related_name='characters')
+
+    def get_absolute_url(self):
+        return reverse('character-detail', kwargs={'pk': self.pk})
+
+
+class CharacterForm(ModelForm):
+    class Meta:
+        model = Character
+        fields = ['page_name', 'display_name', 'caption', 'desc', 'sm_icon', 'lg_icon']
+
+
+class Verse(Entity):
+    franchise = models.CharField(max_length=255)
+
+    def get_absolute_url(self):
+        return reverse('verse-detail', kwargs={'pk': self.pk})
+
+
+class VerseForm(ModelForm):
+    class Meta:
+        model = Verse
+        fields = ['display_name', 'caption', 'desc', 'franchise']
+
+
+class Bio(Model):
+    character = models.ForeignKey('Character', related_name='bios', on_delete=models.DO_NOTHING)
+    verse = models.ForeignKey('Verse', related_name='bios', on_delete=models.DO_NOTHING)
+    _display_name = models.CharField(max_length=255, default='', blank=True)
+    _caption = models.CharField(max_length=255, default='', blank=True)
+    _desc = models.TextField(blank=True)
+    _sm_icon = models.URLField(default='', blank=True)
+    _lg_icon = models.URLField(default='', blank=True)
+
+    def display_name(self):
+        if self._display_name != '':
+            return self._display_name
+        return self.character.display_name
+
+    def caption(self):
+        if self._caption != '':
+            return self._caption
+        return self.character.caption
+
+    def desc(self):
+        if self._desc != '':
+            return self._desc
+        return self.character.desc
+
+    def sm_icon(self):
+        if self._sm_icon != '':
+            return self._sm_icon
+        return self.character.sm_icon
+
+    def lg_icon(self):
+        if self._lg_icon != '':
+            return self._lg_icon
+        return self.character.lg_icon
+
+    def get_absolute_url(self):
+        return reverse('bio-detail', kwargs={'pk': self.pk})
+
+
+class BioForm(ModelForm):
+    class Meta:
+        model = Bio
+        fields = ['character', 'verse', '_display_name', '_caption', '_desc', '_sm_icon', '_lg_icon']
+
+
+class Trait(Model):
+    title = models.CharField(max_length=255)
+    content = models.CharField(max_length=255)
+
+    def __str__(self):
+        return '%s - %s' % (self.title, self.content)
+
+    class Meta:
+        abstract = True
+
+
+class CharacterTrait(Trait):
+    char = models.ForeignKey(Character, related_name='traits', on_delete=models.DO_NOTHING, null=True)
+
+
+class VerseTrait(Trait):
+    ver = models.ForeignKey(Verse, related_name='traits', on_delete=models.DO_NOTHING, null=True)
+
+
+class VerseTraitForm(ModelForm):
+    class Meta:
+        model = VerseTrait
+        fields = ['ver', 'title', 'content']
+
+
+class BioTrait(Trait):
+    bio = models.ForeignKey(Bio, related_name='traits', on_delete=models.DO_NOTHING, null=True)
+
+
+class BioTraitForm(ModelForm):
+    class Meta:
+        model = BioTrait
+        fields = ['bio', 'title', 'content']
---
+++ apps/rp/views.py
@@ -0,0 +1,175 @@
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+from django.views.generic.edit import FormView, CreateView, UpdateView, DeleteView
+from .models import *
+from django.forms import Select
+
+
+class CharacterDetailView(DetailView):
+    model = Character
+
+
+class CharacterListView(ListView):
+    model = Character
+
+    def get_queryset(self):
+        return Character.objects.order_by('page_name')
+
+
+class CharacterCreateView(CreateView):
+    model = Character
+    fields = ['page_name', 'display_name', 'caption', 'desc', 'sm_icon', 'lg_icon']
+    template_name = 'rp/form.html'
+    success_url = "/characters/"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'New Character'
+        return context
+
+
+class CharacterUpdateView(UpdateView):
+    model = Character
+    fields = ['page_name', 'display_name', 'caption', 'desc', 'sm_icon', 'lg_icon']
+    template_name = 'rp/form.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # I'd eventually like to have this say "edit character name"
+        context['title'] = 'Edit Character'
+        return context
+
+
+class CharacterDeleteView(DeleteView):
+    pass
+
+
+class VerseDetailView(DetailView):
+    model = Verse
+
+
+class VerseListView(ListView):
+    model = Character
+
+    def get_queryset(self):
+        return Verse.objects.order_by('franchise')
+
+
+class VerseCreateView(CreateView):
+    model = Verse
+    fields = ['display_name', 'franchise', 'caption', 'desc']
+    template_name = 'rp/form.html'
+    success_url = '/verses/'
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'New Verse'
+        return context
+
+
+class VerseUpdateView(UpdateView):
+    model = Verse
+    fields = ['display_name', 'franchise', 'caption', 'desc']
+    template_name = 'rp/form.html'
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['title'] = 'Edit Verse'
+        return context
+
+
+class BioDetailView(DetailView):
+    model = Bio
+
+
+class BioCreateView(CreateView):
+    model = Bio
+    fields = ['character', 'verse', '_display_name', '_caption', '_desc', '_sm_icon', '_lg_icon']
+    template_name = 'rp/form.html'
+    success_url = '/characters/'
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['title'] = 'New Bio'
+        return context
+
+
+class CharacterBioCreateView(CreateView):
+    model = Bio
+    fields = ['character', 'verse', '_display_name', '_caption', '_desc', '_sm_icon', '_lg_icon']
+    template_name = 'rp/form.html'
+    success_url = '/characters/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'New AU'
+        return context
+
+    def get(self, *args, **kwargs):
+        self.initial = {'character': Character.objects.get(pk=kwargs['pk'])}
+        return super().get(self)
+
+
+class VerseBioCreateView(CreateView):
+    model = Bio
+    fields = ['character', 'verse', '_display_name', '_caption', '_desc', '_sm_icon', '_lg_icon']
+    template_name = 'rp/form.html'
+    success_url = '/verses/'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'New Verse'
+        return context
+
+    def get(self, *args, **kwargs):
+        self.initial = {'verse': Verse.objects.get(pk=kwargs['pk'])}
+        return super().get(self)
+
+
+class BioUpdateView(UpdateView):
+    model = Bio
+    fields = ['_display_name', '_caption', '_desc', '_sm_icon', '_lg_icon']
+    template_name = 'rp/form.html'
+    bio = None
+
+    def get(self, *args, **kwargs):
+        self.bio = Bio.objects.get(pk=kwargs['pk'])
+        return super().get(self)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = '%s: <em>%s</em>' % (self.bio.character.display_name, self.bio.verse.display_name)
+        return context
+
+
+class CharacterTraitCreateView(CreateView):
+    model = CharacterTrait
+    fields = ['char', 'title', 'content']
+    template_name='rp/form.html'
+    success_url = '/characters/'
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['title'] = 'New Character Trait'
+        return context
+
+
+class CharacterTraitUpdateView(UpdateView):
+    model = CharacterTrait
+    fields = ['title', 'content']
+    template_name = 'rp/form.html'
+    success_url = '/characters/'
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['title'] = 'Edit Trait'
+        return context
