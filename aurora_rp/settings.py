272239ea9ef21fa71ade5b9be2b6ab6495591526
lyra <darby.vranek@gmail.com>
2018-07-18 20:41:31 -0700

    tried to fix ref to summernote script

--- aurora_rp/settings.py
+++ aurora_rp/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+heroku pg:push rp HEROKU_POSTGRESQL_CHARCOAL -a aurora-rp
 
 django_heroku.settings(locals())
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
