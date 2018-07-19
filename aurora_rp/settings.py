f5994d709f318db77b09138ac36bebe70f69df2c
lyra <darby.vranek@gmail.com>
2018-07-18 20:32:56 -0700

    fixed settings file

--- aurora_rp/settings.py
+++ aurora_rp/settings.py
@@ -128,5 +128,5 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
-# django_heroku.settings(locals())
-# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+django_heroku.settings(locals())
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
