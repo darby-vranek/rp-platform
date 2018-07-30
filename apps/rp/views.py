a2c2c8560adad6cfc327c7bedc5eb7666d4e2927
lyra <darby.vranek@gmail.com>
2018-07-30 15:43:32 -0700

    added print statements

--- apps/rp/views.py
+++ apps/rp/views.py
@@ -286,6 +286,7 @@ class ImageUpdateView(UpdateView):
 
 def sign_s3(request, file_name, file_type, file_type_1):
     s3 = boto3.client('s3')
+    print(s3)
     presigned_post = s3.generate_presigned_post(
         Bucket=os.environ.get('S3_BUCKET_NAME'),
         Key=file_name,
@@ -296,6 +297,7 @@ def sign_s3(request, file_name, file_type, file_type_1):
         ],
         ExpiresIn=3600
     )
+    print(presigned_post)
 
     return json.dumps({
         'data': presigned_post,
