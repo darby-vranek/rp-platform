920e3fd16a14db5ce15b8578cb70ec770c37d1ff
lyra <darby.vranek@gmail.com>
2018-08-06 12:13:15 -0700

    updated some stuff

--- apps/icons/models.py
+++ apps/icons/models.py
@@ -3,29 +3,13 @@ from apps.rp.models import Model
 from django.forms import ModelForm
 
 
-# face claim
-class FaceClaim(Model):
-    name = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.name
-
-
-class FaceClaimForm(ModelForm):
-    class Meta:
-        model = FaceClaim
-        fields = '__all__'
-
-
 # icon
 class Icon(Model):
-    name = models.CharField(max_length=255)
-    fc = models.ForeignKey('FaceClaim', related_name='icons', null=True, blank=True, on_delete=models.DO_NOTHING)
+    fc = models.CharField(max_length=255, default='', blank=True)
     image = models.ImageField(default='admin/img/icon-unknown.svg')
-    title = models.CharField(max_length=255, default='untitled', blank=True)
 
     def __str__(self):
-        return f'Icon name: {self.name}\nFC: {self.fc}\nTitle: {self.title}'
+        return self.image.url
 
 
 class IconForm(ModelForm):
--- apps/icons/urls.py
+++ apps/icons/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.IconListView.as_view(), name='icons'),
-    path('/new/', views.IconCreateView.as_view(), name='new-icon'),
+    path('new/', views.IconCreateView.as_view(), name='new-icon'),
 ]
